4dd7d9fa78898fb490791f2efbf14d5aa1059b22
azad <azad26195@gmail.com>
2016-09-25 15:31:32 +0530

    react-redux added to package setup function added.

--- index.android.js
+++ index.android.js
@@ -3,4 +3,6 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 
+import setup from './src/setup';
+
 AppRegistry.registerComponent('mcq', () => setup);
---
+++ src/setup.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React, { Component } from 'React';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import App from './src/containers/App';
+import configureStore from './src/configureStore'
+
+function setup():React.Component {
+
+    class Root extends Component {
+
+        constructor() {
+            super();
+            this.state = {
+                store: configureStore(()=> this.setState({})),
+            };
+        }
+
+        render() {
+            return (
+                <Provider store={this.state.store}>
+                    <App store={this.state.store} />
+                </Provider>
+            );
+        }
+    }
+    return Root
+}
+
+export default setup;
